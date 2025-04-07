d5c6e6f6df8fe7380fd612806cb80ffa2ec5133c
destroyallsecrets <destroyallsecrets@icloud.com>
2025-04-07 08:22:38 -0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -7,27 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "localhost",
-    port: 8080, // Set port to 8080 as required
-  },
-  optimizeDeps: {
-    exclude: [
-      'strip-ansi-cjs',
-      'wrap-ansi-cjs',
-      'string-width-cjs'
-    ],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 500, // Adjust the chunk size limit if necessary
+    host: "::",
+    port: 8080,
   },
   plugins: [
     react(),
